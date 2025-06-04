27c36662ce10cf32aa66ca25835c9747c6cf605a
Leon <turkov.leon-2@yandex.ru>
2025-06-04 23:12:37 +0300

    style(src): - isort + black style

--- app/common/gateways/urban_api_gateway.py
+++ app/common/gateways/urban_api_gateway.py
@@ -40,15 +40,17 @@ class UrbanAPIGateway:
         )
         gdf = gpd.GeoDataFrame.from_features(resp, crs=4326)
         all_gdfs = []
-        for parent_id in gdf['territory_id']:
+        for parent_id in gdf["territory_id"]:
             child_gdf_resp = await self.api_handler.get(
                 "/api/v1/territory/indicator_values",
                 params={"parent_id": parent_id, "indicator_ids": 1},
             )
             child_gdf = gpd.GeoDataFrame.from_features(child_gdf_resp, crs=4326)
-            child_gdf['parent_territory_id'] = parent_id
-            child_gdf['parent_name'] = gdf.loc[gdf['territory_id'] == parent_id, 'name'].iloc[0]
+            child_gdf["parent_territory_id"] = parent_id
+            child_gdf["parent_name"] = gdf.loc[
+                gdf["territory_id"] == parent_id, "name"
+            ].iloc[0]
             all_gdfs.append(child_gdf)
         res_gdf = pd.concat(all_gdfs, ignore_index=True)
-        res_gdf['population'] = res_gdf['indicators'].apply(lambda x: x[0].get('value'))
+        res_gdf["population"] = res_gdf["indicators"].apply(lambda x: x[0].get("value"))
         return res_gdf
