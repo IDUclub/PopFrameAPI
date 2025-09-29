94db9fbdd8789a3c5e2b3bf92fbcc5ef956bcdef
Leon <100690100+LeonDeTur@users.noreply.github.com>
2025-09-29 18:19:48 +0300

    fix(towns_layers): - fixed towns aggregation for project - added request params to model calculation

--- app/routers/router_popframe_models.py
+++ app/routers/router_popframe_models.py
@@ -5,6 +5,7 @@ from loguru import logger
 
 from app.common.models.popframe_models.popframe_models_service import \
     pop_frame_model_service
+from app.dependencies import towns_layers
 
 recalculating = False
 
@@ -12,16 +13,42 @@ model_calculator_router = APIRouter(prefix="/model_calculator")
 
 
 @model_calculator_router.put("/recalculate/all")
-async def recalculate_all_popframe_models():
-    asyncio.create_task(pop_frame_model_service.load_and_cache_all_models())
+async def recalculate_all_popframe_models(models: bool = True, towns: bool = True):
+    """
+    Recalculate all popframe models and towns
+
+    Params:
+
+        - models (boolean): weather to recalculate models. Defaults to True.
+
+        - towns: (boolean): weather to recalculate towns. Defaults to True.
+    """
+
+    if models:
+        asyncio.create_task(pop_frame_model_service.load_and_cache_all_models())
+    if towns:
+        asyncio.create_task(towns_layers.cache_all_towns())
     return {"msg": "started recalculation"}
 
 
 @model_calculator_router.put("/recalculate/{region_id}")
-async def recalculate_region(region_id: int):
-    """Router recalculates model for region"""
+async def recalculate_region(region_id: int, model: bool = True, towns: bool = True):
+    """
+    Router recalculates model for region and towns recaching
 
-    await pop_frame_model_service.calculate_model(region_id)
+    Params:
+
+    - region_id (int): region id
+
+    - model (boolean): weather to recalculate models. Defaults to True.
+
+    - towns (boolean): weather to recalculate towns. Defaults to True.
+    """
+
+    if model:
+        await pop_frame_model_service.calculate_model(region_id)
+    if towns:
+        await towns_layers.get_towns(region_id, force=True)
     logger.info(f"Successfully calculated model for region with id {region_id}")
     return {"msg": f"successfully calculated model for region with id {region_id}"}
 
